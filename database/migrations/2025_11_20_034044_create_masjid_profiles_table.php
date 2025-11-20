102e9d8707914f54a72e59a8364b51cb82b1b1d5
Nptnpwr <nptnpwr@gmail.com>
2025-11-20 22:08:56 +0700

    Initial commit on AdminPanel

--- database/migrations/2025_11_20_034044_create_masjid_profiles_table.php
+++ database/migrations/2025_11_20_034044_create_masjid_profiles_table.php
@@ -8,23 +8,22 @@ return new class extends Migration
 {
     public function up(): void
     {
-Schema::create('profil_masjid', function (Blueprint $table) {
-    $table->id();
-    $table->string('nama')->nullable();
-    $table->string('alamat')->nullable();
-    $table->text('about_text_1')->nullable();
-    $table->text('about_text_2')->nullable();
-    $table->text('visi')->nullable();
-    $table->text('misi')->nullable();
-    $table->string('about_image')->nullable();
-    $table->integer('capacity')->nullable();
-    $table->integer('year')->nullable();
-    $table->string('routine_activities')->nullable();
-    $table->string('public_info')->nullable();
-    $table->string('whatsapp')->nullable();
-    $table->timestamps();
-});
-
+        Schema::create('profil_masjid', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama')->nullable();
+            $table->string('alamat')->nullable();
+            $table->text('about_text_1')->nullable();
+            $table->text('about_text_2')->nullable();
+            $table->text('visi')->nullable();
+            $table->text('misi')->nullable();
+            $table->string('about_image')->nullable();
+            $table->integer('capacity')->nullable();
+            $table->integer('year')->nullable();
+            $table->string('routine_activities')->nullable();
+            $table->string('public_info')->nullable();
+            $table->string('whatsapp')->nullable();
+            $table->timestamps();
+        });
     }
 
     public function down(): void
